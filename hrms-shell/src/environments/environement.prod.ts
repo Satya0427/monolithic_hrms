99c921a03678574b9eb87bde058f9a334012fd23
manikanta0427 <manikanta.yalla@assettl.com>
2026-06-17 12:58:30 +0530

    domie allo

--- hrms-shell/src/environments/environement.prod.ts
+++ hrms-shell/src/environments/environement.prod.ts
@@ -1,5 +1,5 @@
 export class environment {
   public static production: boolean = false;
-  public static baseApiUrl: string = 'http://100.100.56.54:4201/api';
-  public static apiUrl: string = 'http://100.100.56.54:4201/api'
+  public static baseApiUrl: string = '/api';
+  public static apiUrl: string = '/api'
 }
